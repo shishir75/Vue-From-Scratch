fca13baebe4d2fdf97334075bd6524894f350266
shishir75 <shishir.srdr16@gmail.com>
2019-08-29 02:26:16 +0600

    NPM, Webpack & Single File Component

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import Accordion from './components/Accordion';
+
+
+
+new Vue({
+    el: '#app',
+    components: {
+        Accordion,
+    },
+    data: {
+        items: [
+            { id: 1, title: 'Title 1', description: 'Description for Title 1. ' },
+            { id: 2, title: 'Title 2', description: 'Description for Title 2. ' },
+            { id: 3, title: 'Title 3', description: 'Description for Title 3. ' },
+        ]
+    }
+});
